c17814be34f8ad9d9ff62723c2005bf13fc4b480
Higashigawa Ryo <higashikawap@unzip.co.jp>
2019-04-12 12:59:00 +0900

    ソースミス修正

--- main.js
+++ main.js
@@ -5,16 +5,18 @@ const BrowserWindow = electron.BrowserWindow;
 let mainWindow = null;
 
 app.on('window-all-closed', function() {
-    if (process.platform !== 'win32') {
+    if (process.platform !== 'darwin') {
         app.quit();
     }
 });
 
-app.on('ready', function () {
-    mainWindow = new BrowserWindow({ width: 800, height: 600 });
-    mainWindow.loadURL('file://' + __dirname + 'index.html');
+app.on('ready', function() {
 
-    mainWindow.on('closed', function () {
+    // ブラウザ(Chromium)の起動, 初期画面のロード
+    mainWindow = new BrowserWindow({width: 800, height: 600});
+    mainWindow.loadURL('file://' + __dirname + '/index.html');
+
+    mainWindow.on('closed', function() {
         mainWindow = null;
     });
 });
